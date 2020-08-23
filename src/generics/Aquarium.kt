6a1eb4adb44096832a4b4a8a109d4e70d57656d1
K.V <vishwan@boardpac.co>
2020-08-23 23:43:39 +0530

    in/out in generics added

--- src/generics/Aquarium.kt
+++ src/generics/Aquarium.kt
@@ -19,17 +19,33 @@ class LakeWater : WaterSupply(true){
 //class Aquarium<T: Any?>(val waterSupply: T) //default is T: Any?
 //class Aquarium<T: Any>(val waterSupply: T) // To remove nulls T: Any
 // To specify exact type, add specific type like WaterSupply
-class Aquarium<T: WaterSupply>(val waterSupply: T) {
-    fun addWater(){
+class Aquarium<out T: WaterSupply>(val waterSupply: T) {
+    fun addWater(cleaner: Cleaner<T>){
         check(!waterSupply.needsProcessing){
             "water supply needs processing first"
+        }
+        if(waterSupply.needsProcessing){
+            cleaner.clean(waterSupply)
         }
         println("add water")
     }
 }
 
+fun addToItem(aquarium: Aquarium<WaterSupply>) =  println("item added")
+
+interface Cleaner<in T :WaterSupply>{
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner : Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalCleaners()
+    }
+}
+
 fun genericsExample(){
     val aquarium = Aquarium<TapWater>(TapWater())
+    addToItem(aquarium)
     println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
     aquarium.waterSupply.addChemicalCleaners()
     println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
@@ -40,9 +56,12 @@ fun genericsExample(){
 //    var aquarium3 = Aquarium(null)
 //    println(aquarium3.waterSupply)
 
-    var aquarium4 = Aquarium(LakeWater())
-    aquarium4.waterSupply.filter()
-    println(aquarium4.addWater())
+//    var aquarium4 = Aquarium(LakeWater())
+//    aquarium4.waterSupply.filter()
+//    println(aquarium4.addWater())
+    val aquarium5 = Aquarium(TapWater())
+    var cleaner = TapWaterCleaner()
+    aquarium5.addWater(cleaner)
 }
 
 fun main(){
