417506f9920aeba63eafa86be100bcaca8789b59
K.V <vishwan@boardpac.co>
2020-08-26 22:15:08 +0530

    lambdas, higher-order functions

--- src/other/functional.kt
+++ src/other/functional.kt
@@ -22,6 +22,36 @@ fun main(){
         println(temp)
     }
 
+    fishSample.apply {
+        name = "newName"
+    }
+    println(fishSample.name)
+
+    myWith("test"){
+        println(capitalize())
+    }
+
+    val testLet = fishSample.name.capitalize().let { it+"haha" }.let { it.length }.let{it + 31}
+    println(testLet)
+
+    lateinit var oops:String
+    myWith("oops", object :Function1<String, Unit>{
+        override fun invoke(name: String) {
+            oops = name.capitalize()
+        }
+    })
+    println(oops)
+
+    val runnable = object : Runnable {
+        override fun run() {
+            println("I'm a Runnable")
+        }
+    }
+
+    JavaRun.runNow(runnable)
+    JavaRun.runNow {
+        println("Passing lambda as a Runnable")
+    }
 }
 
 fun labels() {
@@ -36,6 +66,10 @@ fun labels() {
 
 val waterFilter = { dirty: Int -> dirty/2}
 
-data class Fish(val name:String)
+data class Fish(var name:String)
 
 val fishList  = listOf(Fish("Gold fish"), Fish("Dory"), Fish("Sail fish"), Fish("Salmon"))
+
+inline fun myWith(name:String, block: String.() -> Unit){
+    return name.block()
+}
